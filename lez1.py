22c27bd8f4b4c8c55251b87836f4d72685beb264
unknown <gai.omar96@gmail.com>
2021-10-11 09:36:52 +0200

    Prima esercitazione in aula

---
+++ lez1.py
@@ -0,0 +1,25 @@
+if __name__=="__main__":
+    #This is the main
+    print ("Sentence to print")
+    print("Another sentence")  ##no \n is needed
+    
+value = 4.356434
+print("The average value is: %.2f" %value)  ##to print only 2 decimal numbers
+pi=3.15169265
+print(f"{pi:.4}")
+
+##ESERCIZIO 2
+name="Omar"
+age= 25
+birthday= "10/05/1996"
+
+print('My name is ', name, ' and I\'m ', age, ' years old, I was born the ', birthday)
+
+##ESERCIZIO 3
+print("\n\nCompute an addition")
+print(f"2+3={2+3}")
+
+##ESERCIZIO 4
+name=input("What's your name? ")
+print("Hello %s, how are you?" %name)
+

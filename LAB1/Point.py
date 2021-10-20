bc9fc50afce613f2d52342241f4a9025ca1d2a6e
unknown <gai.omar96@gmail.com>
2021-10-20 12:36:12 +0200

    Update on lez1_OOP exercises

--- LAB1/Point.py
+++ LAB1/Point.py
@@ -7,3 +7,12 @@ class Point:
     def distance(self, p):
         dist=math.sqrt((self.x - p.x)**2 + (self.y - p.y)**2)
         return dist 
+
+    def move(self,dx,dy):
+        self.x += dx
+        self.y += dy
+        
+    def getx(self):
+        return self.x
+    def gety(self):
+        return self.y
--- LAB1/lez1_OOP.py
+++ LAB1/lez1_OOP.py
@@ -1,5 +1,7 @@
 from SquareManager import *
 from Point import *
+from Point3D import *
+
 if __name__=="__main__":
     print("SQUARE EXERCISE")
     sm=SquareManager(5)
@@ -13,3 +15,8 @@ if __name__=="__main__":
     b=Point(1,1)
     print(f'distance from a to b is: {a.distance(b)}')
     
+    a.move(8,2)
+    print(f"Move a in ({a.getx()}, {a.gety()})")
+
+    c=Point3D(1,2,1)
+    print(f"Point 3D is in position ({c.getx()},{c.gety()},{c.getz()})")
